18d9565e25ec30b69d576e43e0920118cb241a84
sawabyni <55002841+sawabyni@users.noreply.github.com>
2023-03-06 11:38:33 -0300

    8 commit
    voltando para POST

--- main.py
+++ main.py
@@ -347,7 +347,7 @@ def pix():
 #Recebe as notificacao de pagamento
 token = f"{token} "
 email = f"{e_aut} "
-@app.route('/notificacao', methods=['GET'])
+@app.route('/notificacao', methods=['POST'])
 def notificacao():
     # Recebe a notificação enviada pelo PagSeguro por meio de uma solicitação POST
     xml_notification = request.data.decode('utf-8')
