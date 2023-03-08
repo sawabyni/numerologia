582869f71801a4a58e53aaf23759dffc598cfa1c
sawabyni <55002841+sawabyni@users.noreply.github.com>
2023-03-08 19:06:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -307,7 +307,7 @@ def pagseguro():
     response = requests.post('https://sandbox.api.pagseguro.com/orders', headers=headers, json=payload)
 
     if response.ok:
-        return redirect(url_for('notificacao'))
+        return redirect(url_for('resultado'))
 
         # se o pagamento nao foi precessado com sucesso, retorna JSON response
     return jsonify(response.json())
